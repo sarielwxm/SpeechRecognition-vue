d46d5654373243422039a39fe11ec419b97e8ca9
sarielwxm <40795114+sarielwxm@users.noreply.github.com>
2020-05-08 15:21:44 +0800

    SR

--- src/components/home/Appindex.vue
+++ src/components/home/Appindex.vue
@@ -4,7 +4,7 @@
       <div class="tip">选择文件：</div>
       <input class="file" type="file" title="请选择文件" value="请选择文件">
       <div class="tip">输入邮箱：</div>
-      <input class="inputS" type="text" v-model="mail" placeholder="请在此输入邮箱">
+        <input class="inputS" type="text" v-model="mail" placeholder="请在此输入邮箱">
       <button class="submit" @click="submit">提交</button>
     </div>
   </div>
@@ -24,6 +24,19 @@ export default {
   },
   methods: {
     submit: function () {
+      var regMail = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
+      if (!document.querySelector('input[type=file]').files[0]) {
+        this.$alert('请选择文件')
+        return false
+      }
+      if (!this.mail) {
+        this.$alert('请输入邮箱')
+        return false
+      }
+      if (!regMail.test(this.mail)) {
+        this.$alert('邮箱格式不正确')
+        return false
+      }
       formData.append('file', document.querySelector('input[type=file]').files[0]) // 'file' 这个名字要和后台获取文件的名字一样;
       formData.append('mail', this.mail)
       axios({
@@ -36,6 +49,14 @@ export default {
         }
       }).then((res) => {
         console.log(res.data)
+        if (res.data.code === 200) {
+          this.$alert('发送成功', '提示', {
+            confirmButtonText: '确定',
+            callback: action => {
+              window.location.reload()
+            }
+          })
+        }
       }) // 发送请求
     }
   }
